3cb220ff35654dac18a3bae4ea3576620a2ba48a
pythonian23 <pythonian23@gmail.com>
2025-02-28 00:05:21 +0900

    device-dependent font

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -20,11 +20,16 @@
 ;;
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
-;;
-(setq doom-font (font-spec :family "Fira Code" :size 18)
+
+(if (equal (system-name) "archy")
+    (setq doom-font (font-spec :family "Fira Code" :size 24)
+      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 24)
+      doom-big-font (font-spec :family "Fira Code" :size 48))
+    (setq doom-font (font-spec :family "Fira Code" :size 18)
       doom-variable-pitch-font (font-spec :family "Fira Sans" :size 18)
       doom-big-font (font-spec :family "Fira Code" :size 36))
-;;
+)
+
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
